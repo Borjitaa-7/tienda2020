f9179ae07961d8f8eaa002a9d978a17cc4e5a0ab
Borja <borjiita7@gmail.com>
2020-01-10 10:54:42 +0100

    paso de archivos

---
+++ admin/usuarios/cabecera.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gestión de Alumnado CRUD v.1.1</title>
+        <!--
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <script src="script/jquery.min.js"></script>
+        <script src="script/bootstrap.js"></script>
+        -->
+        <link rel="icon" type="image/png" href="http://localhost/iaw/crudpdo/favicon.png">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+        <link rel="stylesheet" href="http://localhost/iaw/crudpdo/css/print.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+        <style type="text/css">
+            .wrapper{
+                width: 650px;
+                margin: 0 auto;
+            }
+            .page-header h2{
+                margin-top: 0;
+            }
+            table tr td:last-child a{
+                margin-right: 15px;
+            }
+</style>
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            });
+        </script>
+    </head>
+    <body>
+
+<?php require_once "navbar.php"; ?>
---
+++ dirs.php
@@ -0,0 +1,28 @@
+<?php 
+
+// Definimos constantes
+if ( !defined('ROOT_PATH') )
+    define('ROOT_PATH', dirname(__FILE__)."/");
+
+if ( !defined('VIEW_PATH') )
+    define('VIEW_PATH', ROOT_PATH."admin/");
+
+if ( !defined('CONTROLLER_PATH') )
+    define('CONTROLLER_PATH', ROOT_PATH."controladores/");
+
+if ( !defined('MODEL_PATH') )
+    define('MODEL_PATH', ROOT_PATH."modelos/");
+
+if ( !defined('IMAGE_PATH') )
+    define('IMAGE_PATH', ROOT_PATH."productosImagenes/");
+
+if ( !defined('IMAGE_PATH') )
+    define('IMAGE_PATH', ROOT_PATH."usuariosImagenes/");
+
+if ( !defined('UTILITY_PATH') )
+    define('UTILITY_PATH', ROOT_PATH."utilidades/");
+
+if ( !defined('VENDOR_PATH') )
+    define('VENDOR_PATH', ROOT_PATH."vendor/");
+
+?>
