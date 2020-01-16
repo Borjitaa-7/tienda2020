5d41c63531597a0ddf3b5ab85bed3dcb931fb2d7
Borja <borjiita7@gmail.com>
2020-01-16 23:19:03 +0100

    BBDD

--- sql/tienda2020.sql
+++ sql/tienda2020.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 16-01-2020 a las 21:27:45
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.1.32
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-01-2020 a las 23:18:11
+-- Versión del servidor: 10.4.8-MariaDB
+-- Versión de PHP: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,14 +30,33 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articulos` (
   `id` int(11) NOT NULL,
-  `nombre` text NOT NULL,
-  `tipo` text NOT NULL,
-  `distribuidor` text NOT NULL,
-  `precio` text NOT NULL,
-  `descuento` text NOT NULL,
-  `unidades` text NOT NULL,
-  `imagen` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` text COLLATE utf8_spanish_ci NOT NULL,
+  `distribuidor` text COLLATE utf8_spanish_ci NOT NULL,
+  `precio` text COLLATE utf8_spanish_ci NOT NULL,
+  `descuento` text COLLATE utf8_spanish_ci NOT NULL,
+  `unidades` text COLLATE utf8_spanish_ci NOT NULL,
+  `imagen` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `dni` text COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` text COLLATE utf8_spanish_ci NOT NULL,
+  `email` text COLLATE utf8_spanish_ci NOT NULL,
+  `password` text COLLATE utf8_spanish_ci NOT NULL,
+  `admin` text COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` text COLLATE utf8_spanish_ci NOT NULL,
+  `fecha` text COLLATE utf8_spanish_ci NOT NULL,
+  `imagen` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Índices para tablas volcadas
@@ -50,6 +69,12 @@ ALTER TABLE `articulos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -57,7 +82,13 @@ ALTER TABLE `articulos`
 -- AUTO_INCREMENT de la tabla `articulos`
 --
 ALTER TABLE `articulos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
